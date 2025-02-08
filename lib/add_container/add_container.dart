c7ee01202d9510de5a9d52a2ec2b4dde746f5c6a
BluLightning <125098257+BluLightning@users.noreply.github.com>
2025-02-08 13:19:20 -0800

    Here

---
+++ lib/add_container/add_container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddContainer extends StatefulWidget {
+  const AddContainer({super.key});
+
+  @override
+  State<AddContainer> createState() => _AddContainerState();
+}
+
+class _AddContainerState extends State<AddContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
